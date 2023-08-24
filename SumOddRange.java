cb68b7053ceca9605a097bc51647391d29366410
MAKROFREEK <makrofreek@gmail.com>
2023-08-24 07:49:01 -0500

    why-not-worky-question-mark

--- SumOddRange.java
+++ SumOddRange.java
@@ -1,29 +1,30 @@
 public class SumOddRange {
 
     public static void main(String[] args) {
-        System.out.println(sumOdd(1,100));
+        // Problematic Test Cases
+        System.out.println(sumOdd(-1, 100)); // -1 if negative 2500 if positive
+        System.out.println(sumOdd(100, 1000)); // 247500
+        System.out.println(sumOdd(10, 10)); // 0
+        System.out.println(sumOdd(13, 13)); // 13
+        System.out.println(sumOdd(1, -5)); // -1
     }
 
     public static boolean isOdd(int number) {
-        if (number < 0) {
+        if (number < 0 && number % 2 == 0) {
             return false;
-        } else if (number % 2 == 0) {
-            return false;
-        } else {
-            return true;
-        }
+        } return true;
     }
-    //this method does not care for negatives but needs to
-    public static int sumOdd(int start, int end) {
-        int sum = 0;
-        for (int i = 0; i <= end; i++) {
-            if (isOdd(i)) {
-                sum += i;
-            } else if (isOdd(i) == false) {
-                return -1;
-            }
 
-        }
-        return sum;
+    // this method does not care for negatives but needs to1
+    public static int sumOdd(int start, int end) {
+    int sum = 0;
+    if (start > end || start < 0) {
+        return -1;
+    }
+    for (int i = start; i < end; i++) {
+        if (isOdd(i)) {
+                return sum += i;
+            }
+        } return sum;
     }
 }
