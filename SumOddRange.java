9482b6c9fa89e43491f00956c0019dde948b036b
MAKROFREEK <makrofreek@gmail.com>
2023-08-24 18:11:12 -0500

    finally working :cry_face:

--- SumOddRange.java
+++ SumOddRange.java
@@ -10,21 +10,25 @@ public class SumOddRange {
     }
 
     public static boolean isOdd(int number) {
-        if (number < 0 && number % 2 == 0) {
+        if (number < 0) {
             return false;
-        } return true;
+        } else if (number % 2 == 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
-
-    // this method does not care for negatives but needs to1
+    //finally got this right
     public static int sumOdd(int start, int end) {
-    int sum = 0;
-    if (start > end || start < 0) {
-        return -1;
-    }
-    for (int i = start; i < end; i++) {
-        if (isOdd(i)) {
-                return sum += i;
+        int sum = 0;
+        if (start <= 0 || end <= 0 || start > end) {
+            return -1;
+        }
+        for (int i = start; i <= end; i++) {
+            if (isOdd(i)) {
+                sum += i;
             }
-        } return sum;
+        }
+        return sum;
     }
 }
