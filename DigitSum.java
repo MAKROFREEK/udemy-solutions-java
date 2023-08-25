b54d4e73cf9964d17cfbd67ea61d25dd2a9462a7
MAKROFREEK <makrofreek@gmail.com>
2023-08-25 18:34:16 -0500

    new-test-cases

--- DigitSum.java
+++ DigitSum.java
@@ -1,11 +1,18 @@
 public class DigitSum {
     
     public static void main(String[] args) {
-        
+        sumDigits(1234);
+        System.out.println("");
+        sumDigits(5678);
+        System.out.println("");
+        sumDigits(3939);
     }
 
     public static int sumDigits(int number) {
-        return 0;
+        int numba = number;
+        for (int i = 0; i < 4; i++) {
+            System.out.println(numba % 10);
+            numba /= 10;
+        } return numba;
     }
-
 }
