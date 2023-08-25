91c7a55ee8f9bce0a5b44d922353d18287c4afa1
MAKROFREEK <makrofreek@gmail.com>
2023-08-24 20:42:44 -0500

    accidentally-made-an-infinite-loop-but-fixed-it

--- WhileLoop.java
+++ WhileLoop.java
@@ -1,3 +1,23 @@
 public class WhileLoop {
     
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+        int j = 1;
+        boolean isReady = false;
+
+        do {
+            if (j > 5) {
+                break;
+            }
+            System.out.println(j);
+            j++;
+            isReady = (j > 0);
+        } while (isReady);
+    }
+
+
+
+
 }
