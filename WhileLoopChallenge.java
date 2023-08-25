f72f934dd5589b9167cabe1b40695e037c94657b
MAKROFREEK <makrofreek@gmail.com>
2023-08-24 20:45:05 -0500

    finished-challenge

--- WhileLoopChallenge.java
+++ WhileLoopChallenge.java
@@ -2,13 +2,17 @@ public class WhileLoopChallenge {
     public static void main(String[] args) {
         int i = 5;
         while (i <= 20) {
-            System.out.println(i);
+            if (isEvenNumber(i)) {
+                System.out.println(i);
+            }
             i++;
         }
     }
+
     public static boolean isEvenNumber(int number) {
         if (number % 2 == 0) {
             return true;
-        } return false;
+        }
+        return false;
     }
 }
