6745cb11ab65555286e994b6d5d248cea0568128
MAKROFREEK <makrofreek@gmail.com>
2023-08-26 08:01:39 -0500

    DigitSum-Solution

--- DigitSum.java
+++ DigitSum.java
@@ -1,18 +1,34 @@
 public class DigitSum {
-    
+
+    // This code calls the sumDigits function three times with different numbers as
+    // arguments,
+    // and then prints a space in the output after each function call.
     public static void main(String[] args) {
         sumDigits(1234);
-        System.out.println("");
+        System.out.println(""); // adds a space in the output
         sumDigits(5678);
-        System.out.println("");
+        System.out.println(""); // adds a space in the output
         sumDigits(3939);
     }
 
+    // This code calculates the sum of the individual digits in a given number.
+    // It iteratively divides the number by 10 and prints the remainder (the last
+    // digit) at each step. Finally,
+    // it returns the remaining quotient (the first digit).
     public static int sumDigits(int number) {
         int numba = number;
-        for (int i = 0; i < 4; i++) {
-            System.out.println(numba % 10);
-            numba /= 10;
-        } return numba;
+        if (number < 0) {
+            return -1;
+        } else {
+            int count = 0;
+            for (int i = 0; i < 4; i++) {
+                int digit = numba % 10;
+                System.out.println(numba % 10);
+                numba /= 10;
+                count += digit;
+            }
+            System.out.println(count);
+            return numba;
+        }
     }
 }
