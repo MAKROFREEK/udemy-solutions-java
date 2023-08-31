38e556158f4ecb2f12f9e5644978289e1f599680
MAKROFREEK <84992148+MAKROFREEK@users.noreply.github.com>
2023-08-31 08:30:28 -0500

    LargestPrime - init

---
+++ LargestPrime.java
@@ -0,0 +1,21 @@
+public class LargestPrime {
+
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+        System.out.println(getLargestPrime(217));
+        System.out.println(getLargestPrime(0));
+        System.out.println(getLargestPrime(45));
+        System.out.println(getLargestPrime(-1));
+    }
+
+    public static int getLargestPrime(int n) {
+        int largestPrime = 0;
+        if (n < 0) {
+            return -1;
+        }
+        for (int i = 0; i < n; i++) {
+
+        }
+    }
+
+}
