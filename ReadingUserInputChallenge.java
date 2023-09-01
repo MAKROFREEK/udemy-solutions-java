310c29dcf2693e2ac79c1370ef83216a6d0858ce
MAKROFREEK <MAKROFREEK@gmail.com>
2023-08-31 20:48:51 -0500

    FileDisperser.java - init

---
+++ ReadingUserInputChallenge.java
@@ -0,0 +1,13 @@
+public class ReadingUserInputChallenge {
+    public static void main(String[] args) {
+
+    }pubic
+
+    static int poopooCaca(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i);
+        }
+        int j = 1;
+        boolean isReady = falsej
+    }
+}
