0c1d2185f3304c1b8e13946a36dabfeed162903f
MAKROFREEK <MAKROFREEK@gmail.com>
2023-09-03 13:51:55 -0500

    adopting intellij projects into udemy

---
+++ IdeaProjects/FlowerPacker/src/FlourPacker.java
@@ -0,0 +1,21 @@
+public class FlourPacker {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+
+        //big = 5 kilo
+        //small = 1 kilo
+        //goal = total kilos needed
+
+        int bigCount;
+        int smallCount;
+        int goalCount;
+        for (int i = 0; i < goal; i++) {
+
+            return false;
+        }
+    }
+}
